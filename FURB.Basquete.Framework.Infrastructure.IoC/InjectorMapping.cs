c971684c1d3a942299aa37b022a3ee1963019e52
Isaac Nunes Borges\isaacnborges <isaacnborges@gmail.com>
2018-09-04 20:00:04 -0300

    Criação de projeto para Injeção de Depêndencia(IoC), criação dos controllers de time e jogador, ajuste no .gitignore

---
+++ FURB.Basquete.Framework.Infrastructure.IoC/InjectorMapping.cs
@@ -0,0 +1,36 @@
+﻿using FURB.Basquete.Framework.ApplicationService.Interfaces;
+using FURB.Basquete.Framework.ApplicationService.Services;
+using FURB.Basquete.Framework.Domain.Interfaces.Repositories;
+using FURB.Basquete.Framework.Domain.Interfaces.Services;
+using FURB.Basquete.Framework.Domain.Services;
+using FURB.Basquete.Framework.Infrastructure.Data;
+using FURB.Basquete.Framework.Infrastructure.Interfaces;
+using FURB.Basquete.Framework.Infrastructure.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FURB.Basquete.Framework.Infrastructure.IoC
+{
+    public class InjectorMapping
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            services.AddScoped<IConfig, Config>();
+            services.AddScoped<IConnect, Connect>();
+
+            services.AddScoped<ITimeAppService, TimeAppService>();
+            services.AddScoped<ITemporadaTimeAppService, TemporadaTimeAppService>();
+            services.AddScoped<IJogadorAppService, JogadorAppService>();
+            services.AddScoped<ITemporadaJogadorAppService, TemporadaJogadorAppService>();
+
+            services.AddScoped<ITimeService, TimeService>();
+            services.AddScoped<ITemporadaTimeService, TemporadaTimeService>();
+            services.AddScoped<IJogadorService, JogadorService>();
+            services.AddScoped<ITemporadaJogadorService, TemporadaJogadorService>();
+
+            services.AddScoped<ITimeRepository, TimeRepository>();
+            services.AddScoped<ITemporadaTimeRepository, TemporadaTimeRepository>();
+            services.AddScoped<IJogadorRepository, JogadorRepository>();
+            services.AddScoped<ITemporadaJogadorRepository, TemporadaJogadorRepository>();
+        }
+    }
+}
--- FURB.Basquete.Framework.Web/Startup.cs
+++ FURB.Basquete.Framework.Web/Startup.cs
@@ -1,15 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using FURB.Basquete.Framework.ApplicationService.Interfaces;
-using FURB.Basquete.Framework.ApplicationService.Services;
-using FURB.Basquete.Framework.Domain.Interfaces.Repositories;
-using FURB.Basquete.Framework.Domain.Interfaces.Services;
-using FURB.Basquete.Framework.Domain.Services;
-using FURB.Basquete.Framework.Infrastructure.Data;
-using FURB.Basquete.Framework.Infrastructure.Interfaces;
-using FURB.Basquete.Framework.Infrastructure.Repositories;
+﻿using FURB.Basquete.Framework.Infrastructure.IoC;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -40,23 +29,12 @@ namespace FURB.Basquete.Framework.Web
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
-            services.AddScoped<IConfig, Config>();
-            services.AddScoped<IConnect, Connect>();
+            RegisterServices(services);
+        }
 
-            services.AddScoped<ITimeAppService, TimeAppService>();
-            services.AddScoped<ITemporadaTimeAppService, TemporadaTimeAppService>();
-            services.AddScoped<IJogadorAppService, JogadorAppService>();
-            services.AddScoped<ITemporadaJogadorAppService, TemporadaJogadorAppService>();
-
-            services.AddScoped<ITimeService, TimeService>();
-            services.AddScoped<ITemporadaTimeService, TemporadaTimeService>();
-            services.AddScoped<IJogadorService, JogadorService>();
-            services.AddScoped<ITemporadaJogadorService, TemporadaJogadorService>();
-
-            services.AddScoped<ITimeRepository, TimeRepository>();
-            services.AddScoped<ITemporadaTimeRepository, TemporadaTimeRepository>();
-            services.AddScoped<IJogadorRepository, JogadorRepository>();
-            services.AddScoped<ITemporadaJogadorRepository, TemporadaJogadorRepository>();
+        private static void RegisterServices(IServiceCollection services)
+        {
+            InjectorMapping.RegisterServices(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
